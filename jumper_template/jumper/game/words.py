de9a88ec206a16ecfe6a1097211ad228dba4399d
Kevin <kevinnjones117@gmail.com>
2021-05-21 19:25:31 -0600

    Removed the swears and added comments

--- jumper_template/jumper/game/words.py
+++ jumper_template/jumper/game/words.py
@@ -1,15 +1,33 @@
 import random
 
 class Words():
+    """A code template for a list of words.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        console (Console): An instance of the class of objects known as Console.
+        keep_playing (boolean): Whether or not the game can continue.
+        jumper (Jumper): An instance of the class of objects known as Jumper.
+        words (Words): An instance of the class of objects known as Words.
+    """
     
 
     def __init__(self):
+        """The words class constructor.
+        
+        Args:
+            self (words): an instance of words.
+        """
         self.word_list = self.__load_words()
         self.word = self.get_word()
-        # self.word = "apple"
         self.current_word = [ "_" for x in range(len(self.word))]
 
     def __load_words(self):
+        """
+        Loads the word file into a list and returns it. 
+        """
         with open("jumper_template/jumper/game/words.txt") as word_list:
             word_list = word_list.read()
             word_list = word_list.split()
@@ -22,6 +40,7 @@ class Words():
         return random.choice(self.word_list)
 
     def show_lines(self):
+        """returns a string with the number of characters """
         lines = ""
         for x in self.current_word:
             lines += x + " "
@@ -29,6 +48,7 @@ class Words():
 
 
     def get_lines(self, guess):
+        """ Updates the current word with the correct guess."""
         if guess in self.word:
             
             for i in range(len(self.word)):
@@ -39,25 +59,9 @@ class Words():
             return False
     
     def check_for_dashes(self):
-        """Read previous line"""
+        """Checks for dashes"""
 
         if not "_" in self.current_word:
             return True
         else:
             return False
-            
-        
-        
-        
-
-# words = Words()
-# for x in words.current_word:
-#     print(x, end=" ")
-# print()
-# guess =""
-# while guess != "quit":
-#     guess = input("What's your guess? ")
-#     words.get_lines(guess)
-#     for x in words.current_word:
-#         print(x, end=" ")
-#     print()
